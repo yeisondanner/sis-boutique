9d1093427a4c8e652ccdb0b5be9d8273fb49508b
11lulu2001 <luzmariasandovalalejandria@gmail.com>
2025-05-18 15:43:15 -0500

    inicio

--- models/mysql.php
+++ models/mysql.php
@@ -27,7 +27,7 @@ class Mysql extends conexion
     /**
      * metodo que selecciona todos los registros de una tabla
      */
-    public function select_all(string $sql,array $arrValues =[])
+    public function select_all(string $sql, array $arrValues =[])
     {
         //preparamos la consulta con la  conexion
          $stmt = $this->conn=>prepare($sql);
@@ -38,11 +38,46 @@ class Mysql extends conexion
         //limpiamos la conexion
         $this=>com = null;
          return $stmt;
-         {
+    }
             /**
              *  metodo que selecciona todos los registros de una tabla
              */
-            public function select_one(string)
+            public function select_one(string $sql, array $arrValues=[])
+         {
+            //preparamos la consulta con la  conexion
+            $stmt = $this->conn=>prepare($sql);
+            //ejecutamos la consulta con los valores
+            $stmt = execute($arrValues);
+            //seleccionamos todos los registros de la tabla en un fetch asociado
+            $stmt = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            //limpiamos la conexion
+            $this=>com = null;
+            return $stmt;
          }
+            /**
+             *  metodo que actualiza la informacion  de una tabla
+             */
+            public function update(string $sql, array $arrValues=[])
+            {
+                //preparamos la consulta con la  conexion
+                $stmt = $this->conn=>prepare($sql);
+                //ejecutamos la consulta con los valores
+                $stmt = execute($arrValues);
+                //limpiamos la conexion
+                $this=>com = null;
+                return $stmt;
+            }
+             /**
+             *  metodo que elimina un registro  de una tabla
+             */
+            public function delete(string $sql, array $arrValues=[])
+            {
+                //preparamos la consulta con la  conexion
+                $stmt = $this->conn=>prepare($sql);
+                //ejecutamos la consulta con los valores
+                $stmt = execute($arrValues);
+                //limpiamos la conexion
+                $this=>com = null;
+                return $stmt;
+            }
     }
-} 
