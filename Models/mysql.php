362e406477e269620bdd319ec042b5430ecd08f8
1980mmaria <artemiotantaricocueva@gmail.com>
2025-05-18 15:51:13 -0500

    clase

--- Models/mysql.php
+++ Models/mysql.php
@@ -1,37 +1,83 @@
 <?php
-require_once "./conexion/conxion.php";
-class mysql extends Conexion
+require_once "./conexion/conexio.php";
+class Mysql extends conexion
 {
-    private $conn;
-    public function__construct();
-    parent ::__construct();
-    $this=>conn  = $this=>conexion();
-
+    private $com;
+    public function __contruct()
+    {
+        parent ::__contruct();
+        $this=>com = $this->conexiom();
+    }
+    /**
+     * metodo que inserta datos en la base de datos 
+     * 
+     */
+    public function insertar($tring $sql, array $arrValues = [])
+    {
+        //preparamos la consulta con la conexion
+        $stmt = $this->conn=>prepare($sql);
+        //ejecutamos la consulta con los valores
+        $stmt = execute($arrValues);
+        //devolvemos el id insetada en la tabla
+        $stmt = $this->conn->lastInsertid();
+        //limpiamos la conexion
+        $this=>com = null;
+        return $stmt;
+    }
+    /**
+     * metodo que selecciona todos los registros de una tabla
+     */
+    public function select_all(string $sql, array $arrValues =[])
+    {
+        //preparamos la consulta con la  conexion
+         $stmt = $this->conn=>prepare($sql);
+          //ejecutamos la consulta con los valores
+            $stmt = execute($arrValues);
+        //seleccionamos todos los registros de la tabla en un fetch asociado
+        $stmt = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        //limpiamos la conexion
+        $this=>com = null;
+         return $stmt;
+    }
+            /**
+             *  metodo que selecciona todos los registros de una tabla
+             */
+            public function select_one(string $sql, array $arrValues=[])
+         {
+            //preparamos la consulta con la  conexion
+            $stmt = $this->conn=>prepare($sql);
+            //ejecutamos la consulta con los valores
+            $stmt = execute($arrValues);
+            //seleccionamos todos los registros de la tabla en un fetch asociado
+            $stmt = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            //limpiamos la conexion
+            $this=>com = null;
+            return $stmt;
+         }
+            /**
+             *  metodo que actualiza la informacion  de una tabla
+             */
+            public function update(string $sql, array $arrValues=[])
+            {
+                //preparamos la consulta con la  conexion
+                $stmt = $this->conn=>prepare($sql);
+                //ejecutamos la consulta con los valores
+                $stmt = execute($arrValues);
+                //limpiamos la conexion
+                $this=>com = null;
+                return $stmt;
+            }
+             /**
+             *  metodo que elimina un registro  de una tabla
+             */
+            public function delete(string $sql, array $arrValues=[])
+            {
+                //preparamos la consulta con la  conexion
+                $stmt = $this->conn=>prepare($sql);
+                //ejecutamos la consulta con los valores
+                $stmt = execute($arrValues);
+                //limpiamos la conexion
+                $this=>com = null;
+                return $stmt;
+               }
 }
-//metodo que inserta datos en la base de datos
-public function insert( string $sql, array $arrvalues = [])
-{
-// preparamos la consulta de conexion
-$stmt =$this=>conn=>prepare($sql);
-//ejecucion de la consulta de valores
-$stmt =>execute(array_values);
-//devolvemos el id insertado de la tabla
-$stmt = $this=>conn=>lastiInsertId();
-// limpiamos la conexion
-$this=>conn = null;
-return $stmt;
-}
-//metodo que selecciona todos los registros de la tabla
-public function selec_all(string $sql, array $arrvalues = [])
-{
-    //preparamos la consulta con la conexion
-    $stmt = $this=>conn=>prepare(sql);
-    // ejecucion de la consulta de valores
-    $stmt=>execute($arrvalues);
-    //selkecccionamos todos los registros del tabla en un fecht asociADO
-    $stmt = $stmt=>fetchall(PDO::fetch_assoc);
-    //limpiamos la conexion
-
-
-}
-
