136274f42da43f6b033dda8bad58afded941f60e
yeisondanner <yeisoncarhuapoma@gmail.com>
2025-05-25 09:04:55 -0500

    creation view 404

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-echo "Hello World";
+//validamos que exista la variable get view
+if (!isset($_GET["view"])) {
+    require_once "./Views/App/404/404.php";
+    die();
+}
+//validacion que no este vacia la variable get view
+if (empty($_GET["view"])) {
+    require_once "./Views/App/404/404.php";
+    die();
+}
