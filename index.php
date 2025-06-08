7e169b078a006d8f0415e7ea8577b1c942ffbfd0
yeisondanner <yeisoncarhuapoma@gmail.com>
2025-06-08 09:21:27 -0500

    correccion

--- index.php
+++ index.php
@@ -1,37 +1,36 @@
 <?php
+//validamos que exista la variable get view
 if (!isset($_GET["view"])) {
     $_GET["view"] = "login";
 }
 //validacion que no este vacia la variable get view
-if (!empty($_GET["view"])) {
+if (empty($_GET["view"])) {
     $_GET["view"] = "login";
 }
-
-//almacenamiento en la variable la valor de la variable get view
+//Almancenamos en la variable el valor de la variable get view
 $view = $_GET["view"];
 switch ($view) {
     case 'login':
         $data["id"] = 1;
-        $data["title"] = "inicio de sesion";
-        $data["description"] = "aqui te permite el ingreso al sistema";
-        $data["container"] = "login";
-        $data["view"] = "Login";
+        $data["title"] = "Inicio de sesion";
+        $data["description"] = "Aqui te permite el ingreso al sistema";
+        $data["container"] = "Login";
+        $data["view"] = "login";
         $data["css"] = "login";
         $data["js"] = "login";
-        require_once "./views/App/Login/login.php";
+        require_once "./Views/App/Login/login.php";
         break;
-    case "dashboard":
+    case 'dashboard':
         $data["id"] = 2;
         $data["title"] = "Panel de control";
-        $data["description"] = "Gestion del sistema de control";
+        $data["description"] = "Gestion del sistema de boutique";
         $data["container"] = "Dashboard";
-        $data["wiew"] = "Dashboard";
+        $data["view"] = "dashboard";
         $data["css"] = "dashboard";
         $data["js"] = "dashboard";
         require_once "./Views/App/Dashboard/dashboard.php";
         break;
-
     default:
-        require_once "./views/App/404/404.php";
+        require_once "./Views/App/404/404.php";
         break;
 }
