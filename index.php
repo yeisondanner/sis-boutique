dcb3430201614098834448a0f6f318f94a84b825
yeisondanner <yeisoncarhuapoma@gmail.com>
2025-06-08 09:27:08 -0500

    correccion de dashboard

--- index.php
+++ index.php
@@ -1,40 +1,36 @@
 <?php
-//validamos que exista la variable wiew
+//validamos que exista la variable get view
 if (!isset($_GET["view"])) {
     $_GET["view"] = "login";
-    
 }
-//validamos que la variable wiew no este vacia
+//validacion que no este vacia la variable get view
 if (empty($_GET["view"])) {
     $_GET["view"] = "login";
-    
 }
-//Almacenamos en la variedad el valor de la variable get view
+//Almancenamos en la variable el valor de la variable get view
 $view = $_GET["view"];
 switch ($view) {
-    case "login":
+    case 'login':
         $data["id"] = 1;
         $data["title"] = "Inicio de sesion";
         $data["description"] = "Aqui te permite el ingreso al sistema";
         $data["container"] = "Login";
-        $data["wiew"] = "login";
+        $data["view"] = "login";
         $data["css"] = "login";
         $data["js"] = "login";
         require_once "./Views/App/Login/login.php";
         break;
-        
-    default:
-        require_once "./Views/App/404/404.php";
-        break;
-
-    case "Dashboard":
+    case 'dashboard':
         $data["id"] = 2;
         $data["title"] = "Panel de control";
-        $data["description"] = "Gestion del sistema de control";
+        $data["description"] = "Gestion del sistema de boutique";
         $data["container"] = "Dashboard";
-        $data["wiew"] = "Dashboard";
+        $data["view"] = "dashboard";
         $data["css"] = "dashboard";
         $data["js"] = "dashboard";
         require_once "./Views/App/Dashboard/dashboard.php";
+        break;
+    default:
+        require_once "./Views/App/404/404.php";
         break;
 }
--- views/App/Dashboard/dashboard.php
+++ views/App/Dashboard/dashboard.php
@@ -1,14 +1,15 @@
-¡<!DOCTYPE html>
+¡
+<!DOCTYPE html>
 <html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <título>Dashboard</title>
-    <!-- Bootstrap CSS -->
-    <enlace href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"; rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crosorigin="anonymous">
-    <!-- Bootstrap Icons -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"&gt;
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" ; rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crosorigin="anonymous">
+        <!-- Bootstrap Icons -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
 
 <body>
@@ -36,7 +37,7 @@
         </div>
     </div>
     <!-- Opción 1: Bootstrap Bundle con Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js";integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtuVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crosorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" ;integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtuVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crosorigin="anonymous"></script>
 </body>
 
 </html>
