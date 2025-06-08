86d49cd0e9337585af856e9dbdbd962c9d064ae7
1980mmaria <artemiotantaricocueva@gmail.com>
2025-06-08 11:31:22 -0500

    dashboard

---
+++ Views/Template/start.php
@@ -0,0 +1,11 @@
+<?php
+require_once "./header.php"; ?>
+ <div class="container-fluid">
+        <div class="row flex-nowrap">
+           <!-- nav bar latera-->
+            
+              <?php require_once "./views/Template/navlateral"; ?>
+            <!--Contendor principal-->
+            <div class="col py-3">
+                <!--Nav bar-->
+                 <?php require_once "./views/Template/navbar.php"; ?>
