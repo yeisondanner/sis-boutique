051f5a6c1667802350c1503af6bc0ad13ca0c6a9
Dilmer-Br <bernasantosdilmer@gmail.com>
2025-06-22 15:17:09 -0500

    Tabla de modificaciones

--- views/Template/footer.php
+++ views/Template/footer.php
@@ -1,5 +1,7 @@
 <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<!-- js de la pagina -->
+<script src="<?= BASE_URL ?>/wiews/app/<?= $data["container"] ?>/libraries/css/<?= $data["css"] ?>.css">"></script>
 </body>
 
 </html>
