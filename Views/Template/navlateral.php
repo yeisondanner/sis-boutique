1fbb55b60af207a9188dc7a8699ef10d69af25c0
yeisondanner <yeisoncarhuapoma@gmail.com>
2025-08-03 09:04:46 -0500

    actualziacion de infromacion

--- Views/Template/navlateral.php
+++ Views/Template/navlateral.php
@@ -9,25 +9,31 @@
         <ul class="nav nav-pills flex-column mb-sm-auto 
                 mb-0 align-items-center align-items-sm-start w-100" id="menu">
             <li class="nav-item w-100">
-                <a href="http://" class="nav-link text-white active px-3">
+                <a href="<?= BASE_URL ?>?view=dashboard" class="nav-link text-white active px-3">
                     <i class="bi bi-house-door me-2"></i>
                     <span class="d-none d-sm-inline">Inicio</span>
                 </a>
             </li>
             <li class="nav-item w-100">
-                <a href="http://" class="nav-link text-white px-3">
-                    <i class="bi bi-speedometer2 me-2"></i>
-                    <span class="d-none d-sm-inline">Dashboard</span>
+                <a href="<?= BASE_URL ?>?view=products" class="nav-link text-white px-3">
+                    <i class="bi bi-folder me-2"></i>
+                    <span class="d-none d-sm-inline">Gestion de productos</span>
                 </a>
             </li>
             <li class="nav-item w-100">
-                <a href="http://" class="nav-link text-white px-3">
-                    <i class="bi bi-gear me-2"></i>
-                    <span class="d-none d-sm-inline">Configuracion</span>
+                <a href="<?= BASE_URL ?>?view=customers" class="nav-link text-white px-3">
+                    <i class="bi bi-folder me-2"></i>
+                    <span class="d-none d-sm-inline">Gestion de clientes</span>
                 </a>
             </li>
             <li class="nav-item w-100">
-                <a href="http://" class="nav-link text-white px-3">
+                <a href="<?= BASE_URL ?>?view=sales" class="nav-link text-white px-3">
+                    <i class="bi bi-folder me-2"></i>
+                    <span class="d-none d-sm-inline">Gestion de ventas</span>
+                </a>
+            </li>
+            <li class="nav-item w-100">
+                <a href="<?= BASE_URL ?>" class="nav-link text-white px-3">
                     <i class="bi bi-arrow-right me-2"></i>
                     <span class="d-none d-sm-inline">Salir</span>
                 </a>
